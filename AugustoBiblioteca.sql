e0b1bd1b75eab33479c0bffe3029d48a748594b4
AugustoHenriqueFagundes <gutoadel@outlook.com>
2024-09-12 20:21:03 -0300

    Add files via upload

--- AugustoBiblioteca.sql
+++ AugustoBiblioteca.sql
@@ -60,3 +60,56 @@ INSERT INTO emprestimos (livro_id, nome_emprestador, data_emprestimo, data_devol
 (2, 'Bob Santos', '2024-09-10', '2024-09-25'),
 (3, 'Carla Oliveira', '2024-09-15', NULL);
 
+DELIMITER 
+
+CREATE PROCEDURE adicionar_autor(
+    IN p_nome VARCHAR(100),
+    IN p_data_nascimento DATE,
+    IN p_nacionalidade VARCHAR(50)
+)
+BEGIN
+    INSERT INTO autores (nome, data_nascimento, nacionalidade)
+    VALUES (p_nome, p_data_nascimento, p_nacionalidade);
+END;
+
+DELIMITER 
+
+CREATE PROCEDURE adicionar_livro(
+    IN p_titulo VARCHAR(150),
+    IN p_autor_id INT,
+    IN p_categoria_id INT,
+    IN p_data_publicacao DATE,
+    IN p_isbn VARCHAR(13)
+)
+BEGIN
+    INSERT INTO livros (titulo, autor_id, categoria_id, data_publicacao, isbn)
+    VALUES (p_titulo, p_autor_id, p_categoria_id, p_data_publicacao, p_isbn);
+END;
+
+CREATE PROCEDURE registrar_emprestimo(
+    IN p_livro_id INT,
+    IN p_nome_emprestador VARCHAR(100),
+    IN p_data_emprestimo DATE
+)
+BEGIN
+    INSERT INTO emprestimos (livro_id, nome_emprestador, data_emprestimo)
+    VALUES (p_livro_id, p_nome_emprestador, p_data_emprestimo);
+    
+    -- Atualizar a disponibilidade do livro
+    UPDATE livros
+    SET disponivel = FALSE
+    WHERE id = p_livro_id;
+END;
+
+CREATE FUNCTION verificar_disponibilidade(p_livro_id INT)
+RETURNS BOOLEAN
+BEGIN
+    DECLARE disponibilidade BOOLEAN;
+
+    SELECT disponivel INTO disponibilidade
+    FROM livros
+    WHERE id = p_livro_id;
+
+    RETURN disponibilidade;
+END;
+
